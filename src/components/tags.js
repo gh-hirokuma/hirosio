cdfbc42c191e688ebcf807de35191d117305e1a5
hiroshige.negishi <hirosige1@gmail.com>
2020-03-22 12:59:12 +0700

    update

--- src/components/tags.js
+++ src/components/tags.js
@@ -1,7 +1,9 @@
 import React from "react"
 
 const Tags = ({ node, className }) => (
-  <section className={className ? className : "p-4 bg-white flex flex-wrap"}>
+  <section
+    className={className ? className : "p-4 bg-white flex flex-wrap rounded-b"}
+  >
     {node.frontmatter.tags &&
       node.frontmatter.tags.map(tag => (
         <span className="bg-gray-300 text-gray-700 py-1 px-4 rounded-full m-1">
